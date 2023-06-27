50c44006dbb68c5f66abec027d0559d2a2f63e56
kietnguyeni3 <104881547+kietnguyeni3@users.noreply.github.com>
2023-06-27 17:06:13 +0700

    ad

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 def AddNumbers(a, b):
     return a + b
 
-
 def subtract_numbers(a, b):
     return a - b
 
@@ -9,5 +8,7 @@ def subtract_numbers2(a, b):
     return a - b
 
 print(AddNumbers(5, 10))
+
 print(subtract_numbers(20, 8))
+
 print(subtract_numbers2(1, 2))
